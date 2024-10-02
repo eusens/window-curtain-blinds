2f1b245c000b3ab2416bfe63b5c9172bbb1ac095
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2024-10-02 12:37:00 +0800

    third times

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -62,6 +62,7 @@ function Calendar({
       components={{
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
