b1ca9f4a18ead8da2958ebb24ea1e6e4c4df3ddc
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2024-11-11 09:01:06 +0800

    no indexed issue fixed

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -10,7 +10,6 @@ import PropertyDetails from '@/components/properties/PropertyDetails';
 import UserInfo from '@/components/properties/UserInfo';
 import Description from '@/components/properties/Description';
 import { Separator } from '@/components/ui/separator';
-// import Amenities from '@/components/properties/Amenities';
 import dynamic from 'next/dynamic';
 import { Skeleton } from '@/components/ui/skeleton';
 import ContactAction from '@/components/contact/ContactAction';
@@ -25,6 +24,7 @@ const DynamicMap = dynamic(
     loading: () => <Skeleton className='h-[400px] w-full' />,
   }
 );
+
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const property = await fetchPropertyDetails(params.id);
   if (!property) return { title: 'Property Not Found' };
@@ -39,6 +39,7 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
     },
   };
 }
+
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect('/');
@@ -46,41 +47,40 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
   const profileImage = property.profile.profileImage;
+
   return (
     <section>
+      <Head>
+        <meta name="robots" content="index,follow" />
+      </Head>
       <BreadCrumbs name={property.name} />
       <header className='flex justify-between items-center mt-4'>
         <h1 className='text-4xl font-bold '>{property.tagline}</h1>
         <div className='flex items-center gap-x-4'>
-          {/* share button */}
           <ShareButton name={property.name} propertyId={property.id} />
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
       <ImageContainer mainImage={property.image} name={property.name} />
       <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
-    <div className='lg:col-span-8'>
-      <div className='flex gap-x-4 items-center'>
-        <h1 className='text-xl font-bold'>{property.name}</h1>
-        <PropertyRating inPage propertyId={property.id} />
-      </div>
-      {/* <PropertyDetails details={details} /> */}
-      <p className='text-lg font-semibold mt-4'>
-         {formatCurrency(property.price)} per piece
-      </p>
-      {/* <UserInfo profile={{ firstName, profileImage }} /> */}
-      <Separator className='mt-4' />
-      <Description description={property.description} />
-      <ContactAction  />
-      {/* <Amenities amenities={property.amenities} /> */}
-      {/* <DynamicMap countryCode={property.country} /> */}
-    </div>
-    <div className='lg:col-span-4 flex flex-col items-center'>
-      {/* calendar */}
-       <BookingCalendar />
-    </div>
-  </section>
-    </section>      
+        <div className='lg:col-span-8'>
+          <div className='flex gap-x-4 items-center'>
+            <h1 className='text-xl font-bold'>{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+          <p className='text-lg font-semibold mt-4'>
+            {formatCurrency(property.price)} per piece
+          </p>
+          <Separator className='mt-4' />
+          <Description description={property.description} />
+          <ContactAction />
+        </div>
+        <div className='lg:col-span-4 flex flex-col items-center'>
+          <BookingCalendar />
+        </div>
+      </section>
+    </section>
   );
 }
+
 export default PropertyDetailsPage;
