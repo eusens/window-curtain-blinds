9338d2a7895189cd2a3afaf3c2b2922da23de45d
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2024-11-15 20:23:20 +0800

    add layout Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from '@/components/navbar/Navbar';
 import Providers from './providers';
 import { ClerkProvider } from '@clerk/nextjs';
 import Footer from '@/components/footer/footer';
+import { Analytics } from "@vercel/analytics/react"
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -24,6 +26,7 @@ export default function RootLayout({
           <Providers>
             <Navbar />
             <main className='container py-10'>{children}</main>
+            <Analytics />
             <Footer />
           </Providers>
         </body>
