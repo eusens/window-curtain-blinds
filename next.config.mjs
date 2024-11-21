53cbd51b5d5893a16cd57e2818af64d66670f901
Steven Qi <stevenqi@StevendeMacBook-Air.local>
2024-11-21 10:14:54 +0800

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.clerk.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'tvgxenufqrnhvhpnhglq.supabase.co',
-        },
-      ],
-    },
-    async redirects() {
-      return [
-        {
-          source: '/sitemap.xml', // This is the URL you want to serve the sitemap at
-          destination: '/api/sitemap.xml', // The actual API route serving the sitemap
-          permanent: true, // This is a permanent redirect
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tvgxenufqrnhvhpnhglq.supabase.co',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/sitemap.xml', // This is the URL you want to serve the sitemap at
+        destination: '/api/sitemap.xml', // The actual API route serving the sitemap
+        permanent: true, // This is a permanent redirect
+      },
+    ];
+  },
+};
+
+export default nextConfig;
